a6a1f3e6d83b88895460063ebe2d3e3ee1becd44
CT <ct@CTs-MacBook-Air.local>
2023-02-10 17:23:30 +0300

    version 6 Hello

---
+++ example001_Hello/example005_IfOrElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alina")
+{
+    Console.WriteLine ("Ура, это же Alina!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
